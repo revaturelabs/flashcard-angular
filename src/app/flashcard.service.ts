812aa2f0a0dd3d4e525ac0ea251c9e2661c0f996
rorr <rlayneorr@aim.com>
2017-11-10 15:02:17 -0500

    stuff

--- src/app/flashcard.service.ts
+++ src/app/flashcard.service.ts
@@ -8,7 +8,9 @@ import { Flashcard } from './flashcard';
 
 const FLASHCARDS: Flashcard[] = [
   {id: 1, question: 'What is Angular', answer: 'IDK', category: 'Angular', status: 'Approved'},
-  {id: 2, question: 'What is a directive', answer: 'IDK', category: 'Angular', status: 'Approved'}
+  {id: 2, question: 'What is a directive', answer: 'IDK', category: 'Angular', status: 'Approved'},
+  {id: 3, question: 'What is a directive', answer: 'IDK', category: 'JavaScript', status: 'Approved'},
+  {id: 4, question: 'What is a directive', answer: 'IDK', category: 'Servlets', status: 'Approved'}
 ];
 
 @Injectable()
--- src/app/study-mode/study-mode.component.ts
+++ src/app/study-mode/study-mode.component.ts
@@ -1,15 +1,53 @@
 import { Component, OnInit } from '@angular/core';
 
+import { CategoryService } from '../category.service';
+import { FlashcardService } from '../flashcard.service';
+
+import { Flashcard } from '../flashcard';
+
 @Component({
   selector: 'app-study-mode',
   templateUrl: './study-mode.component.html',
   styleUrls: ['./study-mode.component.css']
 })
 export class StudyModeComponent implements OnInit {
+  private categories: string[];
+  private studyCats: string[];
+  private studying: boolean;
+  private fc: Flashcard;
+  private flashcards: Flashcard[];
 
-  constructor() { }
+  constructor(
+    private categoryService: CategoryService,
+    private flashcardService: FlashcardService
+  ) { }
 
   ngOnInit() {
+    this.categoryService.getCategories()
+    .subscribe(categories => this.categories = categories);
+    this.studyCats = [];
+    this.studying = false;
+    this.flashcards = [];
+    this.fc = { id: 0, question: '', answer: '', category: '', status: ''};
   }
 
+  updateList(event, cat): void {
+    if (this.studyCats.indexOf(cat) !== -1) {
+      this.studyCats.splice(this.studyCats.indexOf(cat), 1);
+    } else {
+      this.studyCats.splice(this.studyCats.push(cat));
+    }
+  }
+
+  study(categories): void {
+    this.studying = true;
+    this.studyCats.forEach(cat => this.flashcardService.getFlashcardsByCategory(cat)
+    .subscribe(fcs => fcs.forEach(fc => this.flashcards.push(fc))));
+    console.log(this.flashcards);
+    this.fc = this.flashcards[Math.floor(Math.random() * this.flashcards.length)];
+  }
+
+  next(): void {
+    this.fc = this.flashcards[Math.floor(Math.random() * this.flashcards.length)];
+  }
 }
